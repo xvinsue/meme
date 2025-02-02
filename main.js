34610a40db39c203f108d6ca3b3ca59cb5a74824
vinsu <136402729+xvinsue@users.noreply.github.com>
2025-02-02 11:13:10 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// scripts/main.js
+
+// Add interactivity to the "Get Started" button
+const ctaButton = document.getElementById("ctaButton");
+
+ctaButton.addEventListener("click", () => {
+  alert("Thank you for getting started!");
+});
